8d5dc3564588ec361bb4fcc8917b2c3db826a344
titanium <titaniumdev123@gmail.com>
2022-05-04 13:16:10 +0000

    homepage completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:my_weather_app/app.dart';
 
 void main() => runApp(
-  DevicePreview(
-    enabled: true,
-    builder: (context) => const MyApp(), // Wrap your app
-  ),
+  const MyApp()
 );
