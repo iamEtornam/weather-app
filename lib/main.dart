907a8c2b7b1ef8f8f9429b97033120b9e6484187
titanium <titaniumdev123@gmail.com>
2022-05-04 12:52:04 +0000

    homepage complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:my_weather_app/app.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: true,
+    builder: (context) => const MyApp(), // Wrap your app
+  ),
+);
--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,82 +1,177 @@
 import 'dart:math';
 
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final pages = [], currentIndex = 1;
+
+  _handleIndexChanged(int index){
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFF7F7F7),
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: const [
-                Icon(Icons.location_on),
-                Text(
-                  'ACCRA',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22,
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              const SizedBox(height: 35),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.location_on),
+                  Text(
+                    'ACCRA',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Text('${DateTime.now()}'),
+              const SizedBox(height: 56),
+              Card(
+                elevation: 2,
+                shadowColor: Colors.grey,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            '23°',
+                            style: TextStyle(
+                              fontSize: 65,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            'Real Feel: 23',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                            ),
+                          )
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.sunny_snowing, size: 65),
+                          Text(
+                            'Sunny',
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          )
+                        ],
+                      )
+                    ],
                   ),
                 ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Text('${DateTime.now()}'),
-            Card(
-              elevation: 2,
-              shadowColor: Colors.grey,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                child: Column(
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          '23°',
-                          style: TextStyle(
-                            fontSize: 65,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          'Real Feel: 23',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                          ),
-                        )
-                      ],
+                    WeatherDetailLabel(
+                      label: 'Humidity',
+                      value: '${Random().nextInt(100)}%',
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Icon(Icons.sunny_snowing, size: 65),
-                        Text(
-                          'Sunny',
-                          style: TextStyle(fontWeight: FontWeight.w500),
-                        )
-                      ],
-                    )
+                    const SizedBox(height: 15),
+                    const WeatherDetailLabel(
+                      label: 'UV index',
+                      value: '',
+                    ),
+                    const SizedBox(height: 15),
+                    const WeatherDetailLabel(
+                      label: 'Wind Speed',
+                      value: '5.00 mph',
+                    ),
+                    const SizedBox(height: 15),
+                    WeatherDetailLabel(
+                      label: 'Rain probability',
+                      value: '${Random().nextInt(100)}%',
+                    ),
+                    const SizedBox(height: 15),
+                    const WeatherDetailLabel(
+                      label: 'Pressure',
+                      value: '1023.6 Pa',
+                    ),
                   ],
                 ),
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: DotNavigationBar(
+          marginR: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          currentIndex: currentIndex,
+          dotIndicatorColor: Colors.white,
+          unselectedItemColor: Colors.grey[300],
+          selectedItemColor: const Color(0xFF8A9DFF),
+          // enableFloatingNavBar: false,
+          onTap: _handleIndexChanged,
+          items: [
+            /// Weather
+            DotNavigationBarItem(
+              icon: Column(
+                children: const [
+                  Icon(Icons.cloud),
+                  Text('Weather', style: TextStyle(fontSize: 12))
+                ],
               ),
             ),
-            const SizedBox(height: 16),
-            Column(
-              children: const [
-                WeatherDetailLabel(
-                  label: 'Humidity',
-                  value: '87%',
-                )
-              ],
-            )
+
+            /// Places
+            DotNavigationBarItem(
+              icon: Column(
+                children: const [
+                  Icon(Icons.location_on),
+                  Text('Places', style: TextStyle(fontSize: 12))
+                ],
+              ),
+            ),
+
+            /// News
+            DotNavigationBarItem(
+              icon: Column(
+                children: const [
+                  Icon(Icons.newspaper_outlined),
+                  Text('News', style: TextStyle(fontSize: 12))
+                ],
+              ),
+            ),
+
+            /// Preference
+            DotNavigationBarItem(
+              icon: Column(
+                children: const [
+                  Icon(Icons.person),
+                  Text('Preference', style: TextStyle(fontSize: 12))
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -99,13 +194,14 @@ class WeatherDetailLabel extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        const Text(
-          'Humidity',
-          style: TextStyle(
+        Text(
+          label,
+          style: const TextStyle(
             fontWeight: FontWeight.w600,
+            color: Color.fromARGB(255, 103, 103, 103)
           ),
         ),
-        Text('${Random().nextInt(100)}%'),
+        Text(value),
       ],
     );
   }
