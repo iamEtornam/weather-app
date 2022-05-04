dd8e29c3a3dca3d1520e46627b4ce8378b64f729
Etornam <sunumacbright@gmail.com>
2022-05-04 15:38:22 +0000

    implement weather func

---
+++ lib/repositories/weather_repository.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:my_weather_app/models/weather.dart';
+
+abstract class WeatherRepository {
+  Future<Weather> getWeather({required double latitude, required double longitude});
+}
+
+class WeatherRepositoryImpl implements WeatherRepository {
+  final url = 'https://api.openweathermap.org/data/2.5/weather';
+
+  String urlBuilder({required lat, required lon}) {
+    const apiKey = String.fromEnvironment('API_KEY');
+    return '$url?lat=$lat&lon=$lon&appid=$apiKey&units=metric';
+  }
+
+  @override
+  Future<Weather> getWeather({required double latitude, required double longitude}) async {
+    try {
+      Response res = await get(Uri.parse(urlBuilder(lat: latitude, lon: longitude)));
+      int statusCode = res.statusCode;
+      if (statusCode == 200) {
+        return Weather.fromJson(json.decode(res.body));
+      } else {
+        throw Exception('Failed to load weather');
+      }
+    } catch (e) {
+      print(e);
+      throw Exception('Failed to load weather');
+    }
+  }
+}
--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,7 +1,10 @@
-import 'dart:math';
-
 import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+import 'package:my_weather_app/models/weather.dart';
+import 'package:my_weather_app/providers/weather_provider.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -11,10 +14,27 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final WeatherProvider _weatherProvider = WeatherProvider();
+  Position? _location;
   final pages = [], currentIndex = 1;
 
-  _handleIndexChanged(int index){
+  _handleIndexChanged(int index) {}
 
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
+  @override
+  void dispose() {
+    _location = null;
+    super.dispose();
+  }
+
+  getCurrentLocation() async {
+    _location = await _weatherProvider.getLocation();
+    setState(() {});
   }
 
   @override
@@ -22,105 +42,134 @@ class _HomeViewState extends State<HomeView> {
     return Scaffold(
       backgroundColor: const Color(0xFFF7F7F7),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            children: [
-              const SizedBox(height: 35),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(Icons.location_on),
-                  Text(
-                    'ACCRA',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Text('${DateTime.now()}'),
-              const SizedBox(height: 56),
-              Card(
-                elevation: 2,
-                shadowColor: Colors.grey,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
+        child: _location == null
+            ? const Center(child: CircularProgressIndicator())
+            : StreamBuilder<Weather>(
+                stream: _weatherProvider.getWeatherInfo(
+                  latitude: _location!.latitude,
+                  longitude: _location!.longitude,
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            '23°',
-                            style: TextStyle(
-                              fontSize: 65,
-                              fontWeight: FontWeight.w600,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting &&
+                      snapshot.data == null) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.done && snapshot.data == null) {
+                    return const Center(child: Text('No data'));
+                  }
+
+                  return Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 35),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(Icons.location_on),
+                            FutureBuilder<List<Placemark>>(
+                                future: placemarkFromCoordinates(
+                                    _location!.latitude, _location!.longitude),
+                                builder: (context, snapshot) {
+                                  final data = snapshot.data ?? [];
+                                  return Text(
+                                    data.isEmpty ? 'N/A' : '${data[0].name}',
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 22,
+                                    ),
+                                  );
+                                }),
+                          ],
+                        ),
+                        const SizedBox(height: 16),
+                        Text(DateFormat('E dd MMM yyyy')
+                            .format(DateTime.fromMillisecondsSinceEpoch(snapshot.data!.dt * 1000))),
+                        const SizedBox(height: 56),
+                        Card(
+                          elevation: 2,
+                          shadowColor: Colors.grey,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(24.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '${snapshot.data!.main!.temp}°',
+                                      style: const TextStyle(
+                                        fontSize: 65,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    Text(
+                                      'Real Feel: ${snapshot.data!.main!.feelsLike}',
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Image.network(
+                                      'http://openweathermap.org/img/wn/${snapshot.data!.weather!.first.icon}@2x.png',
+                                      width: 65,
+                                      height: 65,
+                                    ),
+                                    Text(
+                                      '${snapshot.data!.weather!.first.description}',
+                                      style: const TextStyle(fontWeight: FontWeight.w500),
+                                    )
+                                  ],
+                                )
+                              ],
                             ),
                           ),
-                          Text(
-                            'Real Feel: 23',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                            ),
-                          )
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Icon(Icons.sunny_snowing, size: 65),
-                          Text(
-                            'Sunny',
-                            style: TextStyle(fontWeight: FontWeight.w500),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                child: Column(
-                  children: [
-                    WeatherDetailLabel(
-                      label: 'Humidity',
-                      value: '${Random().nextInt(100)}%',
+                        ),
+                        const SizedBox(height: 16),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                          child: Column(
+                            children: [
+                              WeatherDetailLabel(
+                                label: 'Humidity',
+                                value: '${snapshot.data!.main!.humidity}%',
+                              ),
+                              const SizedBox(height: 15),
+                              const WeatherDetailLabel(
+                                label: 'UV index',
+                                value: '',
+                              ),
+                              const SizedBox(height: 15),
+                              WeatherDetailLabel(
+                                label: 'Wind Speed',
+                                value: '${snapshot.data!.wind!.speed} mph',
+                              ),
+                              const SizedBox(height: 15),
+                              const WeatherDetailLabel(
+                                label: 'Rain probability',
+                                value: 'N/A%',
+                              ),
+                              const SizedBox(height: 15),
+                              WeatherDetailLabel(
+                                label: 'Pressure',
+                                value: '${snapshot.data!.main!.pressure} Pa',
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
                     ),
-                    const SizedBox(height: 15),
-                    const WeatherDetailLabel(
-                      label: 'UV index',
-                      value: '',
-                    ),
-                    const SizedBox(height: 15),
-                    const WeatherDetailLabel(
-                      label: 'Wind Speed',
-                      value: '5.00 mph',
-                    ),
-                    const SizedBox(height: 15),
-                    WeatherDetailLabel(
-                      label: 'Rain probability',
-                      value: '${Random().nextInt(100)}%',
-                    ),
-                    const SizedBox(height: 15),
-                    const WeatherDetailLabel(
-                      label: 'Pressure',
-                      value: '1023.6 Pa',
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
+                  );
+                }),
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.only(bottom: 10),
@@ -197,9 +246,7 @@ class WeatherDetailLabel extends StatelessWidget {
         Text(
           label,
           style: const TextStyle(
-            fontWeight: FontWeight.w600,
-            color: Color.fromARGB(255, 103, 103, 103)
-          ),
+              fontWeight: FontWeight.w600, color: Color.fromARGB(255, 103, 103, 103)),
         ),
         Text(value),
       ],
